04b68d289b5f974b15ae36529ccac3115b94005a
Guilherme Motta Soares <gmotta1206@gmail.com>
2026-06-12 21:07:56 +0000

    função de salvar

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+#importações da bibliotecas
 import os
 from datetime import datetime as dt
 import json
 
+#Anira eu aceito
 def salvarLog(criado, removido):
     os.makedirs("log", exist_ok=True)
     log_file ="log/log.json"
@@ -20,24 +22,3 @@ def salvarLog(criado, removido):
     with open(log_file, "w", encoding="utf-8") as F:
         json.dump(logs, F, indent=4, unsure_ascii = False) 
     
-
-
-
-
-
-
-
-for (root,dirs,files) in os.walk('https://github.com/gmottagit/repoteste/tree/main',topdown=True):
-    if files == "log.json":
-        os.file.remove #ignorar
-
-    else:
-        if dirs.__len__ != 0:
-            true = os.path.exists(".gitkeep")
-            if true:
-                files.remove(".gitkeep")
-            
-        else:
-            os.path.join("gitkeep")
-        
-        os.path.join()
